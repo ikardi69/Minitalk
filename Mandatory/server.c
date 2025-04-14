7ccb8e69425c00359c66d97b983a03cd5ab0d3cb
ikardi <mohssine.teffahibs@gmail.com>
2025-04-14 21:40:41 +0100

    fixing

--- Mandatory/server.c
+++ Mandatory/server.c
@@ -6,7 +6,7 @@
 /*   By: mteffahi <mteffahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:16:24 by mteffahi          #+#    #+#             */
-/*   Updated: 2025/04/14 18:29:53 by mteffahi         ###   ########.fr       */
+/*   Updated: 2025/04/14 21:35:06 by mteffahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	signal_handler(int sig)
 	static int				i = 0;
 	static unsigned char	c = 0;
 
+    sigset_t blockmask, oldmask;
+    sigemptyset(&blockmask);
+    sigaddset(&blockmask, SIGUSR1);
+    sigaddset(&blockmask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &blockmask, &oldmask);
 	if (sig == 	SIGUSR2)
 		c |= (1 << (7 - i));
 	i++;
@@ -27,6 +32,7 @@ void	signal_handler(int sig)
 		i = 0;
 		c = 0;
 	}
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
 
 int	main(int argc, char **argv)
@@ -44,10 +50,12 @@ int	main(int argc, char **argv)
 	// 	return (ft_putstr_fd(2, "Allocation error\n"), 1);
 	sa.sa_handler = signal_handler;
 	sigemptyset(&(sa.sa_mask));
+	sigaddset(&(sa.sa_mask), SIGUSR1);
+	sigaddset(&(sa.sa_mask), SIGUSR2);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGUSR1, &sa, NULL) < 0 ||
-		sigaction(SIGUSR2, &sa, NULL))
-		return (ft_putstr_fd(" Failed tpo set up signal handler\n", 2), 1);
+		sigaction(SIGUSR2, &sa, NULL) < 0)
+		return (ft_putstr_fd(" Failed to set up signal handler\n", 2), 1);
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr(getpid());
 	ft_putchar('\n');
